f79960ccff855b684169e8cb16735c229825ea7d
Iktiup-Mahmud <seyam.official96@gmail.com>
2025-10-21 01:17:23 +0600

    feat:add  structure & basic design

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "University Computer Club",
+  description:
+    "Join our vibrant community of tech enthusiasts, coders, and innovators",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
